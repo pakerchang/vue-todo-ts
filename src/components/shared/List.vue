db7033cf0ffe762c5117782d47cbdcd4139cb750
pakerchang <pakerchang@gmail.com>
2022-12-13 22:07:13 +0800

    chore(comment) fix word.

--- src/components/shared/List.vue
+++ src/components/shared/List.vue
@@ -8,7 +8,7 @@ const props = defineProps({
 <template>
   <div class="w-1/3 h-full flex-col shadow-xl rounded-lg">
     <h2 class="text-center font-bold py-2 shadow-sm rounded-lg bg-gray-200">{{ title }}</h2>
-    <!-- How to bind map key for typescript ? -->
+    <!-- How to bind map key in typescript ? -->
     <div class="h-8 flex px-4 justify-between items-center my-3" v-for="(item, index) in data" :key:string="item">
       {{ item }}
       <div>
