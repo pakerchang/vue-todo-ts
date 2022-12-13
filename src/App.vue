6e109e2344f91ca19dca503cf435bd250972e23c
paker_z <99458393+paker-zhang@users.noreply.github.com>
2022-12-13 16:51:21 +0800

    feat(components): Add public layout.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import LayoutPublic from "@/components/layout/LayoutPublic.vue";
 
 </script>
 
 <template>
-
+  <LayoutPublic>
+  </LayoutPublic>
 </template>
---
+++ src/components/layout/LayoutPublic.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="w-screen h-screen flex flex-col items-center bg-gray-100">
+    <slot></slot>
+  </div>
+</template> 
