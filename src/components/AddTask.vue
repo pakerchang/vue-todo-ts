fc84075811133a8e973007c390e8ecd8565a35d7
pakerchang <pakerchang@gmail.com>
2022-12-13 20:42:23 +0800

    feat(Components): Add task bar.

---
+++ src/components/AddTask.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import LayoutAddTask from './layout/LayoutAddTask.vue';
+import { PlusIcon } from '@heroicons/vue/24/outline'
+</script>
+
+<template>
+  <LayoutAddTask>
+    <h1>Add Task</h1>
+    <div class="">
+      <input class="rounded-lg px-3 py-2 focus:outline-none" type="text" placeholder="Add a new task" />
+      <button class="pr-3 w-6 h-6" type="submit">
+        <PlusIcon class="w-6 h-6" />
+      </button>
+    </div>
+
+  </LayoutAddTask>
+</template>
---
+++ src/components/layout/LayoutAddTask.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="w-1/3 rounded-md shadow-xl bg-white mt-16">
+    <slot></slot>
+  </div>
+</template>
