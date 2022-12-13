7be57daf63b471b4cc598306b5eea64759ebd014
pakerchang <pakerchang@gmail.com>
2022-12-13 20:49:52 +0800

    style(component): Adjustment task bar styles.

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -5,13 +5,9 @@ import { PlusIcon } from '@heroicons/vue/24/outline'
 
 <template>
   <LayoutAddTask>
-    <h1>Add Task</h1>
-    <div class="">
-      <input class="rounded-lg px-3 py-2 focus:outline-none" type="text" placeholder="Add a new task" />
-      <button class="pr-3 w-6 h-6" type="submit">
-        <PlusIcon class="w-6 h-6" />
-      </button>
-    </div>
-
+    <input class="w-full rounded-lg px-3 py-2 focus:outline-none" type="text" placeholder="Add a new task" />
+    <button class="w-6 h-6 mr-3 hover:rounded-lg hover:bg-gray-100" type="submit">
+      <PlusIcon class="w-6 h-6" />
+    </button>
   </LayoutAddTask>
 </template>
